70dd4bbbe12b9dab51e439899c00f195ad5904b7
adamskey <adam.lennerling@gmail.com>
2022-11-21 14:58:07 +0100

    update

--- Component/Movie/Movie-Rating/js.js
+++ Component/Movie/Movie-Rating/js.js
@@ -1,27 +1,32 @@
+let ratedStars = 0;
 
-
-function ratingOnClick(id){
-   const n = parseInt(id);
-        for (i=1; i <= n; i++){
-            document.getElementById(i).src="star.png";
-        }
-        for (i=n+1; i < 6; i++){
-            document.getElementById(i).src="star-gray.png";
-        }
-    
+function ratingOnClick(element) {
+    let value = element.getAttribute('id');
+    const n = parseInt(value);
+    lightAllStarsUpTo(n);
     document.getElementById("result").innerHTML = "You rated " + n + " star";
+    ratedStars = n;
+}
+function lightAllStarsUpTo(n) {
+    for (i = 1; i <= n; i++) {
+        document.getElementById(i).src = "star.png";
+    }
+    for (i = n + 1; i < 6; i++) {
+        document.getElementById(i).src = "star-gray.png";
+    }
 }
 
-function rated(){
-  
-  
+function hover(id) {
+    lightAllStarsUpTo(id);
 }
 
-function unrated(id){
-    
+function unhover() {
+    lightAllStarsUpTo(ratedStars);
 }
 
-function resetStars(){
-    for(i=1; i < 6; i++)
-    document.getElementById(i).src="star-gray.png";
+function resetStars() {
+    for (i = 1; i < 6; i++)
+        document.getElementById(i).src = "star-gray.png";
+        document.getElementById("result").innerHTML = " Please rate the movie.";
+        ratedStars = 0;
 }
